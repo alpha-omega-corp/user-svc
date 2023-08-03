938cd9c8a48d5dd75a267d2c85d124b4768addc2
nanstis <bleyo@alphomega.org>
2023-08-03 21:44:26 +0200

    feat: removed migrations

--- Makefile
+++ Makefile
@@ -1,12 +1,6 @@
 server:
 	go run cmd/main.go
 
-db_init:
-	go run cmd/migrations/main.go db init
-
-db_reset:
-	go run cmd/migrations/main.go db reset
-
 proto:
 	protoc --go_out=. \
 		--go_opt=paths=source_relative \
--- pkg/services/server.go
+++ pkg/services/server.go
@@ -3,8 +3,8 @@ package services
 import (
 	"context"
 	"fmt"
-	"github.com/alpha-omega-corp/authentication-svc/pkg/models"
 	"github.com/alpha-omega-corp/authentication-svc/pkg/proto"
+	"github.com/alpha-omega-corp/services/models"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/uptrace/bun"
 	"golang.org/x/crypto/bcrypt"
